701fc8a112380b646ca8f32ddc419869c96ffd72
Nathan <nreinhardt1234@gmail.com>
2018-02-14 11:42:17 -0800

    worked on routes and controllers

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others')
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+module.exports.about = function(req, res){
+  res.render('index', { title: 'About' });
 });
 
 module.exports = router;
