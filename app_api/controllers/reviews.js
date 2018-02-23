058ade8b608b51e25f5463ea5174b284eadc0dee
Nathan <nreinhardt1234@gmail.com>
2018-02-22 20:07:32 -0800

    added chapter 6 content

---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+
+var sendJsonResponse = function(res, status, content) {
+  res.status(status);
+  res.json(content);
+};
+
+module.exports.reviewCreate = function (req, res) {};
