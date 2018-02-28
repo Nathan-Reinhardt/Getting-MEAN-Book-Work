2b406dcc61ace5ed7702959e974e924b82a730c1
Nathan <nreinhardt1234@gmail.com>
2018-02-28 13:16:37 -0800

    chapter 7 content added

--- app_api/models/locations.js
+++ app_api/models/locations.js
@@ -1,10 +1,11 @@
 var mongoose = require( 'mongoose' );
 
 var reviewSchema = new mongoose.Schema({
-  author: String,
-  rating: {type: Number, required: true, min: 0, max: 5},
-  reviewText: String,
-  createdOn: {type: Date, "default": Date.now}
+    author: {type: String, required: true},
+    rating: {type: Number, required: true, min: 0, max: 5},
+    reviewText: {type: String, required: true},
+    createdOn: {type: Date, "default": Date.now
+    }
 });
 
 var openingTimeSchema = new mongoose.Schema({
--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -69,6 +69,22 @@ var _formatDistance = function (distance) {
   return numDistance + unit;
 };
 
+var _showError = function (req, res, status) {
+  var title, content;
+  if (status === 404) {
+    title = "404, page not found";
+    content = "Oh dear. Looks like we can't find this page. Sorry.";
+  } else {
+    title = status + ", something's gone wrong";
+    content = "Something, somewhere, has gone just a little bit wrong.";
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title : title,
+    content : content
+  });
+};
+
 var renderDetailPage = function (req, res, locDetail) {
   res.render('location-info', {
     title: locDetail.name,
@@ -81,8 +97,7 @@ var renderDetailPage = function (req, res, locDetail) {
   });
 }
 
-/* GET 'Location info' page */
-module.exports.locationInfo = function(req, res){
+var getLocationInfo = function (req, res, callback) {
   var requestOptions, path;
   path = "/api/locations/" + req.params.locationid;
   requestOptions = {
@@ -94,21 +109,71 @@ module.exports.locationInfo = function(req, res){
     requestOptions,
     function(err, response, body) {
       var data = body;
-      data.coords = {
-        lng : body.coords[0],
-        lat : body.coords[1]
-      };
-      renderDetailPage(req, res, data);
+      if (response.statusCode === 200) {
+        data.coords = {
+          lng : body.coords[0],
+          lat : body.coords[1]
+        };
+        callback(req, res, data);
+      } else {
+        _showError(req, res, response.statusCode);
+      }
     }
   );
-}
+};
+
+/* GET 'Location info' page */
+module.exports.locationInfo = function(req, res){
+  getLocationInfo(req, res, function(req, res, responseData) {
+    renderDetailPage(req, res, responseData);
+  });
+};
+
+var renderReviewForm = function (req, res, locDetail) {
+  res.render('location-review-form', {
+    title: 'Review ' + locDetail.name + ' on Loc8r',
+    pageHeader: { title: 'Review ' + locDetail.name },
+    error: req.query.err
+  });
+};
 
 /* GET 'Add review' page */
-module.exports.addReview = function(req, res) {
-    res.render('location-review-form', {
-        title: 'Review Starcups on Loc8r',
-        pageHeader: {
-            title: 'Review Starcups'
+module.exports.addReview = function(req, res){
+  getLocationInfo(req, res, function(req, res, responseData) {
+    renderReviewForm(req, res, responseData);
+  });
+};
+
+/* POST 'Add review' page */
+module.exports.doAddReview = function(req, res){
+  var requestOptions, path, locationid, postdata;
+  locationid = req.params.locationid;
+  path = "/api/locations/" + locationid + '/reviews';
+  postdata = {
+    author: req.body.name,
+    rating: parseInt(req.body.rating, 10),
+    reviewText: req.body.review
+  };
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "POST",
+    json : postdata
+  };
+  if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+    res.redirect('/location/' + locationid + '/reviews/new?err=val');
+  } else {
+    request(
+      requestOptions,
+      function(err, response, body) {
+        if (response.statusCode === 201) {
+          res.redirect('/location/' + locationid);
+        } else if (response.statusCode === 400 && body.name && body.name === "ValidationError" ) {
+          res.redirect('/location/' + locationid + '/reviews/new?err=val');
+        } else {
+          console.log(body);
+          _showError(req, res, response.statusCode);
         }
-    });
+      }
+    );
+  }
 };
