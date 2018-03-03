6b8d8db2d69ba7c7caceb5f611e66e630500e880
Nathan <nreinhardt1234@gmail.com>
2018-03-02 17:30:09 -0800

    chapter 8 skeleton and fixed modules

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -1,10 +1,10 @@
 angular.module('loc8rApp', []);
 
-var _isNumeric = function (n) {
+function _isNumeric (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-var formatDistance = function () {
+function formatDistance () {
   return function (distance) {
     var numDistance, unit;
     if (distance && _isNumeric(distance)) {
@@ -22,7 +22,7 @@ var formatDistance = function () {
   };
 };
 
-var ratingStars = function () {
+function ratingStars () {
   return {
     scope: {
       thisRating : '=rating'
@@ -31,23 +31,59 @@ var ratingStars = function () {
   };
 };
 
-var locationListCtrl = function ($scope) {
-  $scope.data = {
-    locations: [{
-      name: 'Burger Queen',
-      address: '125 High Street, Reading, RG6 1PS',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      distance: '0.296456',
-      _id: '5370a35f2536f6785f8dfb6a'
-    },{
-      name: 'Costy',
-      address: '125 High Street, Reading, RG6 1PS',
-      rating: 5,
-      facilities: ['Hot drinks', 'Food', 'Alcoholic drinks'],
-      distance: '0.7865456',
-      _id: '5370a35f2536f6785f8dfb6a'
-    }]};
+function geolocation () {
+  var getPosition = function (cbSuccess, cbError, cbNoGeo) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
+    }
+    else {
+      cbNoGeo();
+    }
+  };
+  return {
+    getPosition : getPosition
+  };
+};
+
+function locationListCtrl ($scope, loc8rData, geolocation) {
+  $scope.message = "Checking your location";
+
+  $scope.getData = function (position) {
+    var lat = position.coords.latitude,
+        lng = position.coords.longitude;
+    $scope.message = "Searching for nearby places";
+    loc8rData.locationByCoords(lat, lng)
+      .success(function(data) {
+        $scope.message = data.length > 0 ? "" : "No locations found nearby";
+        $scope.data = { locations: data };
+      })
+      .error(function (e) {
+        $scope.message = "Sorry, something's gone wrong, please try again later";
+      });
+  };
+
+  $scope.showError = function (error) {
+    $scope.$apply(function() {
+      $scope.message = error.message;
+    });
+  };
+
+  $scope.noGeo = function () {
+    $scope.$apply(function() {
+      $scope.message = "Geolocation is not supported by this browser.";
+    });
+  };
+
+  geolocation.getPosition($scope.getData,$scope.showError,$scope.noGeo);
+};
+
+var loc8rData = function ($http) {
+  var locationByCoords = function (lat, lng) {
+    return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=20000');
+  };
+  return {
+    locationByCoords : locationByCoords
+  };
 };
 
 angular
@@ -55,3 +91,5 @@ angular
   .controller('locationListCtrl', locationListCtrl);
   .filter('formatDistance', formatDistance);
   .directive('ratingStars', ratingStars);
+  .service('loc8rData', loc8rData);
+  .service('geolocation', geolocation);
