0a864f7d07a4132e3f631beb612dc29b98fe2407
Nathan <nreinhardt1234@gmail.com>
2018-03-22 16:32:35 -0700

    pushing to heroku debugging

--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -4,20 +4,21 @@
     .module('loc8rApp')
     .controller('locationDetailCtrl', locationDetailCtrl);
 
-    locationDetailCtrl.$inject = ['$routeParams', '$ubiModal', 'loc8rData'];
-    function locationDetailCtrl ($routeParams, $ubiModal, loc8rData) {
-      var vm = this;
-      vm.locationid = $routeParams.locationid;
-      loc8rData.locationById(vm.locationid)
-        .success(function(data) {
-          vm.data = { location: data };
-          vm.pageHeader = {
-            title: vm.data.location.name
-          };
-        })
-        .error(function (e) {
-          console.log(e);
-        });
+  locationDetailCtrl.$inject = ['$routeParams', '$ubiModal', 'loc8rData'];
+  function locationDetailCtrl ($routeParams, $ubiModal, loc8rData) {
+    var vm = this;
+    vm.locationid = $routeParams.locationid;
+
+    loc8rData.locationById(vm.locationid)
+      .success(function(data) {
+        vm.data = { location: data };
+        vm.pageHeader = {
+          title: vm.data.location.name
+        };
+      })
+      .error(function (e) {
+        console.log(e);
+      });
       vm.popupReviewForm = function () {
         var modalInstance = $ubiModal.open({
           templateUrl: '/reviewModal/reviewModal.view.html',
--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -4,45 +4,45 @@
     .module('loc8rApp')
     .controller('reviewModalCtrl', reviewModalCtrl);
 
-    reviewModalCtrl.$inject = ['$ubiModalInstance', 'loc8rData', 'locationData'];
-    function reviewModalCtrl ($ubiModalInstance, loc8rData, locationData) {
-      var vm = this;
-      vm.locationData = locationData;
+  reviewModalCtrl.$inject = ['$ubiModalInstance', 'loc8rData', 'locationData'];
+  function reviewModalCtrl ($ubiModalInstance, loc8rData, locationData) {
+    var vm = this;
+    vm.locationData = locationData;
 
-      vm.doAddReview = function (locationid, formData) {
-        loc8rData.addReviewById(locationid, {
-          author : formData.name,
-          rating : formData.rating,
-          reviewText : formData.reviewText
-        })
-        .success(function (data) {
-          vm.modal.close(data);
-        })
-        .error(function (data) {
-          vm.formError = "Your review has not been saved, try again";
-        });
+    vm.doAddReview = function (locationid, formData) {
+      loc8rData.addReviewById(locationid, {
+        author : formData.name,
+        rating : formData.rating,
+        reviewText : formData.reviewText
+      })
+      .success(function (data) {
+        vm.modal.close(data);
+      })
+      .error(function (data) {
+        vm.formError = "Your review has not been saved, try again";
+      });
+      return false;
+    };
+
+    vm.onSubmit = function () {
+      vm.formError = "";
+      if (!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
+        vm.formError = "All fields required, please try again";
         return false;
-      };
+      } else {
+        vm.doAddReview(vm.locationData.locationid, vm.formData);
+      }
+    };
 
-      vm.onSubmit = function () {
-        vm.formError = "";
-        if (!vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
-          vm.formError = "All fields required, please try again";
-          return false;
-        } else {
-          vm.doAddReview(vm.locationData.locationid, vm.formData);
-        }
-      };
+    vm.modal = {
+      close : function (result) {
+        $ubiModalInstance.close(result);
+      },
+      cancel : function () {
+        $ubiModalInstance.dismiss('cancel');
+      }
+    };
 
-      vm.modal = {
-        close : function (result) {
-          $modalInstance.close(result);
-        },
-        cancel : function () {
-          $modalInstance.dismiss('cancel');
-        }
-      };
-
-    }
+  }
 
 })();
